b8f54d7084b90c60223c165a3a57431437564caf
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-09-16 20:11:09 +0200

    modal fixed

--- src/components/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../index.css";
 
-const ImageGalleryItem = ({ src, alt }) => (
-    <li className="ImageGalleryItem">
+const ImageGalleryItem = ({ src, alt, onClick }) => (
+    <li className="ImageGalleryItem" onClick={onClick}>
         <img className="ImageGalleryItem-image" src={src} alt={alt} />
   </li>
 )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../index.css";
 
-const Modal = ({ src, alt, onClose }) => (
-  <div className="Overlay" onClick={onClose}>
-    <div className="Modal">
-      <img src={src} alt={alt} />
+const Modal = ({ src, alt, onClose }) => {
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="Overlay" onClick={onClose}>
+      <div className="Modal">
+        <img src={src} alt={alt} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
